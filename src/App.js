59f9691200e9ca599f3e71f9b2e3c8ba27093dc8
Hamza Bukhari <hambukhari@gmail.com>
2022-05-13 17:08:57 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -6,26 +6,24 @@ import { useState } from 'react';
 
 function App() {
 
-  const [arr, setArr] = useState([{ title: "Pudge Arcana", price: 45, image: "./images/pudge.webp", description:"A set of Pudge." }, 
-  { title: "Invoker", price: 65, image: "./images/invoker.jpg", description:"A set of Invoker." },
-   { title: "Ursa", price: 36, image: "./images/ursa.jpg", description:"A set of Ursa Warior." },
-    { title: "Juggernaut", price: 55, image: "./images/juggernaut.jpg", description:"A set of Juggernaut." }, 
-    { title: "Earth Shaker", price: 42, image: "./images/es.jpg" , description:"A set of Earth Shaker."}]);
+  const [arr, setArr] = useState([{id:1, title: "Pudge Arcana", price: 45+"$", image: "./images/pudge.webp", description:"A set of Pudge." ,category: "Strength"}, 
+  {id:2, title: "Invoker", price: 65+"$", image: "./images/invoker.jpg", description:"A set of Invoker.",category: "Intelligence" },
+   {id:3, title: "Ursa", price: 36+"$", image: "./images/ursa.jpg", description:"A set of Ursa Warior.",category: "Strength" },
+    {id:4, title: "Juggernaut", price: 55+"$", image: "./images/juggernaut.jpg", description:"A set of Juggernaut.",category: "Agility" }, 
+    {id:5, title: "Earth Shaker", price: 42+"$", image: "./images/es.jpg" , description:"A set of Earth Shaker.",category: "Strength"}]);
   
-  let quantity = () =>{
-    console.log("Quantity");
-  }
-  let addToCart = () => {
-    console.log("Add to Cart");
-  };
+
+
+
+  
   return (
     <div className="App">
       <div className="heading"><h1>DOTA 2 HEROES</h1></div>
       <header className="App-header">
         
         {
-          arr.map((e) => {
-            return (<Card title={e.title} price={e.price} img={e.image} desc={e.description} action1={addToCart} action2={quantity} />)
+          arr.map((e,index) => {
+            return (<div key={index}><Card title={e.title} price={e.price} img={e.image} desc={e.description} category={e.category} /></div>)
           })
         }
       </header>
--- src/card.js
+++ src/card.js
@@ -1,19 +1,29 @@
 
 function Card(props) {
+  let i =0;
+  const addtocart = ()=>{
+    console.log("Title : "+ props.title + '\n' +
+          "Price : " + props.price + '\n'+
+          "Description : "+ props.desc + '\n'+
+          "Category : "+ props.category + '\n'+
+          "Quantity: ", i++);
+}
+
+
     return (<>
     
       <div className="cards">
         <img src={props.img} />
         <div className="namePrice">
         <h5>Title: {props.title}</h5>
-        <h6>{props.price}$</h6>
+        <h6>{props.price}</h6>
         </div>
         <div>
             <p>Description: {props.desc}</p>
         </div>
         <div className="btns">
-        <button onClick={props.action1}>Add to Cart</button>
-        <button id="plus" onClick={props.action2}>+</button>
+        <button onClick={addtocart}>Add to Cart</button>
+        <button id="plus" onClick={addtocart}>+</button>
         </div>
       </div>
     </>)
